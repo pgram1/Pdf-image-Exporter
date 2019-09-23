4a580d13a5aad594247e62bf5306806a719d3166
Petros Grammatikopoulos <pgrammatikopoulos@citycollege.sheffield.eu>
2019-09-23 11:11:44 +0300

    Windows Compatibility (Libraries not included)
    Adds compatibility for windows as well as a file picker
    
    See releases page for working version with libraries included

--- src/Document.java
+++ src/Document.java
@@ -1,32 +1,31 @@
-package pdfThing;
-
 import java.io.IOException;
 
-public class Document {
 
-	private String path;
-	private String outputFolder;
-	private String outputFile;
 
-	public Document(String path) {
-		this.path = path;
-		
-		//creating a directory name out of a file name by eliminating dots
-		this.outputFolder = (path+"-exports").replace(".", "-");
-		
-		//creating a file name out of the directory name
-		this.outputFile = outputFolder+"/exported";
-	}
-	
-	void getImages() throws IOException{
-			System.out.println("\n-----\nCreating output directory\n-----\n");
-			System.out.println("mkdir "+outputFolder);
-			Runtime.getRuntime().exec("mkdir "+outputFolder);
-			
-			
-			System.out.println("\n-----\nExporting all images as PNG\n-----\n");
-			System.out.println("pdfimages -png "+path+" "+outputFile);
-			Runtime.getRuntime().exec("pdfimages -png "+path+" "+outputFile);
-	}	
+public class Document
+{
+  private String path;
+  private String outputFolder;
+  private String outputFile;
+  
+  public Document(String path) {
+    this.path = path;
 
+    
+    this.outputFolder = (String.valueOf(path) + "-exports").replace(".", "-");
+
+    
+    this.outputFile = String.valueOf(this.outputFolder) + "\\exported";
+  }
+  
+  void getImages() throws IOException {
+    System.out.println("\n-----\nCreating output directory\n-----\n");
+    System.out.println("mkdir.exe \"" + this.outputFolder + "\"");
+    Runtime.getRuntime().exec("3rdbinaries\\mkdir.exe \"" + this.outputFolder + "\"");
+
+    
+    System.out.println("\n-----\nExporting all images as PNG\n-----\n");
+    System.out.println("pdfimages.exe -png \"" + this.path + "\" \"" + this.outputFile + "\"");
+    Runtime.getRuntime().exec("3rdbinaries\\pdfimages.exe -png \"" + this.path + "\" \"" + this.outputFile + "\"");
+  }
 }
